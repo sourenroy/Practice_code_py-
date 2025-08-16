ea8cad03f3c7302759c5a07d7c8d0b6897b98b0f
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-08-16 17:15:25 +0530

    Add files via upload

---
+++ problem18.py
@@ -0,0 +1,18 @@
+s = set() #Empty Set
+
+n = input("Enter the number :")
+s.add(int(n))
+n = input("Enter the number :")
+s.add(int(n))
+n = input("Enter the number :")
+s.add(int(n))
+n = input("Enter the number :")
+s.add(int(n))
+n = input("Enter the number :")
+s.add(int(n))
+n = input("Enter the number :")
+s.add(int(n))
+n = input("Enter the number :")
+s.add(int(n))
+
+print(s)
