a0fe71a7d562a9cb99d21e2d55beb62def03c0cf
sourenroy <168115446+sourenroy@users.noreply.github.com>
2025-08-23 01:54:38 +0530

    Add files via upload

---
+++ problem41.py
@@ -0,0 +1,12 @@
+class Programmer:
+    company = "Google"
+    def __init__(s,name,salary,pin):
+        s.name = name
+        s.salary = salary
+        s.pin = pin 
+
+
+p = Programmer("souren", 1200000, 743390)
+print(p.name, p.salary, p.company)
+r = Programmer("xyz",100 ,700003)
+print(r.name, r.salary, r.company)
